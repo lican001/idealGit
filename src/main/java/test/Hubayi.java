8687585b929f3ea62cde5fc6e353d07b92001f75
wangguangbing <1217268145@qq.com>
2018-01-15 09:38:12 +0800

    第二次

--- src/main/java/test/Hubayi.java
+++ src/main/java/test/Hubayi.java
@@ -1,7 +1,10 @@
 package main.java.test;
 
+import com.Person;
+
 public class Hubayi {
     public static void main(String[] args) {
+        Person person = new Person();
         System.out.println("I for you");
     }
 }
